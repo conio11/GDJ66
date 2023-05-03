2e64d089abd21d5b1170cdae7a3f4c96548b5d5f
gdj66\1 <conio416@naver.com>
2023-05-03 17:18:33 +0900

    board 테이블 컬럼 클래스

---
+++ html_work/userboard/src/main/java/vo/Board.java
@@ -0,0 +1,11 @@
+package vo;
+
+public class Board {
+	public int boardNo;
+	public String localName;
+	public String boardTitle;
+	public String boardContent;
+	public String memberID;
+	public String createdate;
+	public String updatedate;
+}
