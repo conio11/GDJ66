783c05cbf5a7f797a7ea58035196dd0f6e56b205
gdj66\1 <conio416@naver.com>
2023-05-04 17:36:18 +0900

    comment 테이블 컬럼 클래스

---
+++ html_work/userboard/src/main/java/vo/Comment.java
@@ -0,0 +1,10 @@
+package vo;
+
+public class Comment {
+	   public int commentNo;
+	   public int boardNo;
+	   public String commentContent;
+	   public String memberID;
+	   public String createdate;
+	   public String updatedate;
+}
