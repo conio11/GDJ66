80939ca61af914b6449a73fbacce595710ed2632
conio11 <conio416@naver.com>
2023-06-29 17:36:50 +0900

    MVC cashbook 프로젝트

---
+++ html_work/cash/src/main/java/cash/controller/HashtagMain.java
@@ -0,0 +1,14 @@
+package cash.controller;
+
+public class HashtagMain {
+	public static void main(String[] args) {
+		String content = "안녕하세요 #구디아카데미 입니다. 하반기 #자바 교육과정 시간표 공지하였습니다.";
+		
+		/*
+		구디아카데미
+		자바
+		
+		#다음부터 공백 앞까지 자르기
+		*/
+	}
+}
---
+++ html_work/cash/src/main/java/cash/controller/LoginController.java
@@ -0,0 +1,54 @@
+package cash.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import cash.model.*;
+import cash.vo.*;
+
+@WebServlet("/login")
+public class LoginController extends HttpServlet {
+	@Override
+	// private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {		
+		// session 인증 검사 코드
+		HttpSession session = request.getSession();
+		if (session.getAttribute("loginMember") != null) {
+			response.sendRedirect(request.getContextPath() + "/cashbook");
+			return;
+		}
+		
+		// get 방식
+		request.getRequestDispatcher("/WEB-INF/view/login.jsp").forward(request, response);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// doGet(request, response);
+		String memberId = request.getParameter("memberId");
+		String memberPw = request.getParameter("memberPw");
+		
+		Member member = new Member(memberId, memberPw, null, null);
+		
+		MemberDao memberDao = new MemberDao();
+		Member loginMember = memberDao.selectMemberById(member);
+		
+		if (loginMember == null) { 
+			System.out.println("로그인 실패");
+			response.sendRedirect(request.getContextPath() + "/login"); // jsp 파일 리다이렉트 불가 (WEB-INF 폴더 내에 위치하기 때문)
+			return;
+		}
+		
+		// 로그인 성공 시: session 사용
+		HttpSession session = request.getSession();
+		System.out.println("로그인 성공");
+		session.setAttribute("loginMember", loginMember);
+		response.sendRedirect(request.getContextPath() + "/cashbook");
+	}
+}
